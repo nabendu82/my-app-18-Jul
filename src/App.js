7c2ab67f48b359cb4d66360cfbdbedea848eb0c9
nabendu82 <nabendu.biswas@gmail.com>
2022-07-18 12:57:19 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Counter from './components/Counter';
+// import Greeting from './components/Greeting';
+// import { Greet, ReactGreet } from './components/Greet';
+// import Welcome from './components/Welcome';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Greet />
+      <ReactGreet />*/}
+      {/* <Welcome name="Flutter">Language for cross-platform mobile dev</Welcome>
+      <Welcome name="React Native" /> 
+      <Welcome name="Java"><button>Click for Java</button></Welcome>
+      <hr />
+      <Greeting name="JavaScript">Language of the Web</Greeting>
+      <Greeting name="Golang" />
+      <Greeting name="Swift">Language for iOS apps</Greeting> */}
+      <Counter />
     </div>
   );
 }
